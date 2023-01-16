6b44f4d3b0e9d6effc0aaeda4fb0a627361e59ab
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 16:23:28 -0300

    autopep fix task 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,8 @@ else:
     last_digit = number % -10
 
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    print("Last digit of {} is {} and is greater than 5"
+          .format(number, last_digit))
 elif last_digit == 0:
     print("Last digit of {} is {} and is 0".format(number, last_digit))
 elif last_digit < 6:
