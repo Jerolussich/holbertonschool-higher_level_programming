c56947487063182bb44a558ea781ff6a19b3edbd
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 16:30:45 -0300

    task 3 changed format print

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 i = 97
 
-for i in range(97,122):
+for i in range(97, 123):
     if i != 101 and i != 113:
-        print(chr(i), end="")
+        print("{}".format(chr(i)), end="")
