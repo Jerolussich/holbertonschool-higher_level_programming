5a60b98bff81f31ce422a44990bd6451e2e25868
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 20:40:19 -0300

    pep hotfix task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     argument_number = len(sys.argv)
     counter = 1
     sum = 0
-    
+
     for counter in range(1, argument_number):
         sum += int(sys.argv[counter])
     print(sum)
