a77bacb095eb0745cad21cdf8ec7dc277b546562
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 20:38:28 -0300

    task 2 hotfix pep

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -11,9 +11,7 @@ if __name__ == "__main__":
     elif argument_number == 2:
         print("1 argument:\n1: {}".format(sys.argv[counter]))
     elif argument_number > 2:
-        print("{} arguments:".format(argument_number -1))
+        print("{} arguments:".format(argument_number - 1))
         for counter in range(1, argument_number):
             print("{}: {}".format(counter, sys.argv[counter]))
             counter += 1
-    
-
