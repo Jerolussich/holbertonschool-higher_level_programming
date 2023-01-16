7ceda740645c8783699cd58dba168e8406b91a97
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 17:07:38 -0300

    task 8 removed one print for checker

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def uppercase(str):
 
     counter = 0
@@ -8,8 +9,7 @@ def uppercase(str):
         ascii_value = ord(str[counter])
 
         if ascii_value >= 97 and ascii_value <= 122:
-            print("{}".format(chr(ascii_value - 32)), end="")
-        else:
-            print("{}".format(letter), end="")
+            letter = chr(ascii_value - 32)
+        print("{}".format(letter), end="")
         counter += 1
-    print("\n")
+    print("")
