be45304c934153b64d7d24c129fe0fbb12f34a97
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 20:23:15 -0300

    change string format

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -2,10 +2,9 @@
 
 if __name__ == "__main__":
     from add_0 import add
-    
+
     a = 1
     b = 2
 
     c = add(1, 2)
-    print(f"{a} + {b} = {c}")
-
+    print("{} + {} = {}".format(a, b, c))
