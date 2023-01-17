ad10796392002133033556b78eaf4dec2a345400
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 21:01:31 -0300

    hotfix program

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
     for content in names:
-        if content[0:2] != "__":
+        if content[:2] != "__":
             print(content)
