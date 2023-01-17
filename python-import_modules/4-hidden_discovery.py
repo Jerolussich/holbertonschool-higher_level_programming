54847114b21adbb8e63fdd7c0ff79a5b2ddbc17d
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 21:03:28 -0300

    hotfix 2 try

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,6 @@
 
 if __name__ == "__main__":
     import hidden_4
-    names = dir(hidden_4)
-    for content in names:
+    for content in dir(hidden_4):
         if content[:2] != "__":
             print(content)
