bff4e633670638728ed95d631780d6a00c90e968
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-16 21:11:03 -0300

    checker try

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@
 
 if __name__ == "__main__":
     import hidden_4
-    for content in dir(hidden_4):
-        if content[:2] != "__":
-            print(content)
+    for name in dir(hidden_4):
+        if name[:2] != "__":
+            print(name)
