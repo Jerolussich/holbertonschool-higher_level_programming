61f582fac74a13c92b89bc0cd1845799b1afd60c
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-18 11:16:51 -0300

    test for checker

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    names_list = dir(hidden_4)
-    for names in names_list:
-        if (names[:2] != '__'):
-            print(names)
+
+    names = dir(hidden_4)
+    for items in names:
+        if (items[:2] != '__'):
+            print(items)
