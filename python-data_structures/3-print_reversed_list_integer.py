f908ea49038d3e065e370f3837b95ebb05443613
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 11:08:01 -0300

    task 3

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def print_reversed_list_integer(my_list=[]):
+    """print reversed list integer"""
+    size = len(my_list) - 1
+
+    for number in my_list:
+        print("{:d}".format(my_list[size]))
+        size -= 1
