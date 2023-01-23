deba6d0a5be39f68f19bd0fc961a0907232619ff
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 11:10:47 -0300

    bordercase considered

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,8 @@ def print_reversed_list_integer(my_list=[]):
     """print reversed list integer"""
     size = len(my_list) - 1
 
+    if my_list is None:
+        return
     for number in my_list:
         print("{:d}".format(my_list[size]))
         size -= 1
