b2e51277dff065e25e3719ed55c7527531782624
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 20:04:56 -0300

    task 12 edge case hotfix

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,10 @@
 
 
 def roman_to_int(roman_string):
-    """function def roman_to_int(roman_string): 
-    that converts a Roman numeral to an integer"""
+    """function def roman_to_int(roman_string):
+     that converts a Roman numeral to an integer"""
 
-    if roman_string is not str or roman_string is None:
+    if roman_string is None or type(roman_string) is not str:
         return 0
     dict_numerals = {'I': 1, 'V': 5, 'X': 10,
                      'L': 50, 'C': 100, 'D': 500, 'M': 1000}
