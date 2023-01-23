6ead27c1ae3f2cdc3cce592cf43055fb91e4cd27
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 12:09:49 -0300

    task 11 edge cases

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -5,7 +5,7 @@ def delete_at(my_list=[], idx=0):
     """delete list at index"""
     list_size = len(my_list)
 
-    if idx > list_size:
+    if idx >= list_size or idx < 0 or list_size == 0:
         return (my_list)
     del my_list[idx]
     return (my_list)
