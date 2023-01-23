83239c93a5366166bd7fdaa21bdc4881df901abd
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 11:47:44 -0300

    task 7 border case added

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
     """adds tuple ints"""
     size_a = len(tuple_a) - 1
     size_b = len(tuple_b) - 1
 
     a1 = tuple_a[0] if size_a != - 1 else 0
-    a2 = tuple_a[1] if size_a == 1 else 0
+    a2 = tuple_a[1] if size_a > 0 else 0
     b1 = tuple_b[0] if size_b != - 1 else 0
-    b2 = tuple_b[1] if size_b == 1 else 0
+    b2 = tuple_b[1] if size_b > 0 else 0
     return (a1 + b1, a2 + b2)
