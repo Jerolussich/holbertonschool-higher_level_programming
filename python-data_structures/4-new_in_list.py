72d012e8249532f35b3e3839ecaa3cd7698603d8
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 11:15:56 -0300

    task 4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+
+    size = len(my_list) - 1
+
+    if my_list is None:
+        return
+    if idx < 0 or idx > size:
+        return (my_list)
+    else:
+        new_list = my_list[:]
+        new_list[idx] = element
+        return (new_list)
