34aeafad4c50542024e3b51b6afbdf01dfd658d1
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 11:56:14 -0300

    task 8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,6 +4,9 @@
 def multiple_returns(sentence):
     """returns a tuple with the length of a string and its first character"""
     size = len(sentence)
-    first_char = sentence[0] if len != 0 else "None"
 
-    return (size, first_char)
+    if size == 0:
+        return (0, "None")
+    else:
+        first_char = sentence[0]
+        return (size, first_char)
