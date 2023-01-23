35d169f411c3f7d4e01a8edf73f9ce02e5ecc07a
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-23 11:19:14 -0300

    task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    """print matrix integers"""
+    column_idx = 0
+    row_idx = 0
+    size_row = len(matrix) - 1
+    size_column = len(matrix[0]) - 1
+
+    while row_idx <= size_row:
+        while column_idx <= size_column:
+            if column_idx != size_column:
+                print(matrix[row_idx][column_idx], end=" ")
+            else:
+                print(matrix[row_idx][column_idx], end="$\n")
+            column_idx += 1
+        column_idx = 0
+        row_idx += 1
