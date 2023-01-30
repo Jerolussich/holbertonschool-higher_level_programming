1b7ca324ca0b2f9327b28f18845d48c1330593f3
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-30 11:39:22 -0300

    private instance hotfix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -20,7 +20,7 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self._size = value
+        self.__size = value
 
     def area(self):
         """returns areas"""
