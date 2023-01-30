67e14eb75315f6089e52fa686243dd66c37cc5a9
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-30 11:35:54 -0300

    task 4 added

---
+++ python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """initliaze class"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """return area"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._size = value
+
+    def area(self):
+        """returns areas"""
+        return self.__size ** 2
