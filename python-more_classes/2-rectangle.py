1ac5861ef248d97019e6f546a3d1d48f856fc957
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 14:58:24 -0300

    task 2

---
+++ python-more_classes/2-rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class Rectangle:
+    """class rectangle"""
+
+    def __init__(self, __width=0, __height=0):
+        """initializes"""
+        self.__dict__ = {}
+        if type(__height) is not int:
+            raise TypeError("height must be an integer")
+        if __height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = __height
+        if type(__width) is not int:
+            raise TypeError("width must be an integer")
+        if __width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = __width
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("heightmust be >= 0")
+        self.__height = value
+
+    def area(self):
+        """returns rectangle area"""
+        return (self.__height * self.__width)
+
+    def perimeter(self):
+        """returns rectangle perimeter"""
+        return (self.__height * 2 + self.__width * 2)
