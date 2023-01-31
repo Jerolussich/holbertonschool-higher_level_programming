36b1b65290e16abde6584e575dfa5938b713faf0
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 12:23:16 -0300

    task 4 pep and shebang fix

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,7 +1,9 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
+"""shebang"""
+
 
 def print_square(size):
-
+    """print square safely"""
     if size < 0:
         raise ValueError("size must be >= 0")
     if type(size) is not int:
