b1836c21a8759b8d30fc96cb74cd84e4fd3593be
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 11:58:04 -0300

    task 2 checker output fix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-""" Doc """
+"""shebang"""
 
 
 def matrix_divided(matrix, div):
-    """ Doc """
-    if div == float('inf') or div == -float('inf') or div != div:
-        div = 10
+    """divide safely a matrix"""
+    row = len(matrix)
+    row_size = len(matrix[0])
+    new_matrix = [[0 for col in range(row_size)] for row in range(row)]
+
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
-        raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-    for row in matrix:
-        if type(row) is not list:
-            raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
-        if len(row) != len(matrix[0]):
-            raise TypeError("Each row of the matrix must have the same size")
-        for c in row:
-            if type(c) is not int and type(c) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) of \
-integers/floats")
 
-    return [[round(item / div, 2) for item in row] for row in matrix]
+    for i in range(row):
+        if len(matrix[i]) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+
+    for index, item in enumerate(matrix):
+        for index2, sub_item in enumerate(item):
+            if type(sub_item) is not int and type(sub_item) is not float:
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+
+            new_matrix[index][index2] = round((sub_item / div), 2)
+    return (new_matrix)
