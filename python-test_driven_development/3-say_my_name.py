8e1f0828ebcffbed0cdd0c47716e30606fa59dc5
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 12:19:29 -0300

    task 3 pep fix

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""shebang"""
+
 
 def say_my_name(first_name, last_name=""):
-
+    """unites two strings safely"""
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    print(f"{first_name} {last_name}")
+    print(f"My name is {first_name} {last_name}")
