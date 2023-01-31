c5e3774566bcaa2418101908dca4f0c9b47ed7a0
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 13:25:16 -0300

    task 5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -7,6 +7,8 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     for char in text:
+        if char in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+            raise TypeError("text must be a string")
         print(char, end="")
         if char == "." or char == "," or char == "?":
             print("\n")
