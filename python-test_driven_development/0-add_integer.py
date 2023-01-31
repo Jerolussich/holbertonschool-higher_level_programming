a28d71ed7466b3c17551a2d2ba4297c1710dedb6
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 11:04:30 -0300

    task 0 and test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+def add_integer(a, b=98):
+    """add integer safely"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    c = a + b
+    return (c)
