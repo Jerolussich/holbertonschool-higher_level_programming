106cfd615a50ecedd00727a9ac59551dd8456c91
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 12:25:48 -0300

    task 4 and test

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -4,10 +4,10 @@
 
 def print_square(size):
     """print square safely"""
-    if size < 0:
-        raise ValueError("size must be >= 0")
     if type(size) is not int:
         raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
     if size == 0:
         return
     for i in range(size):
