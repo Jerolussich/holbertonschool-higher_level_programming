45b2730094c326fa5071e6d85551a024532f8ab7
Jeronimo Lussich <jerolussich@hotmail.com>
2023-01-31 14:02:02 -0300

    output fix

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,6 +9,16 @@ def text_indentation(text):
     for char in text:
         if char in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
             raise TypeError("text must be a string")
-        print(char, end="")
-        if char == "." or char == "," or char == "?":
+
+        if char == " " and stat == 1:
+            continue
+        elif char.isupper() is True:
+            print(char, end="")
+            stat = 0
+        else:
+            print(char, end="")
+
+        if char == "." or char == "," or char == "?" or char == ":":
             print("\n")
+            stat = 1
+    return ""
