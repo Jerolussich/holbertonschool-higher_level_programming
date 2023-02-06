f87ae5d47349719cb075d5e5e75d9a073d70a643
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-06 16:16:46 -0300

    tasl 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+
+    def area(self):
+        """raises exepction when called"""
+        raise Exception("area is not implemented")
