a9e7b5001aa8696c91d50381e669ba535e894a9e
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-06 16:07:15 -0300

    task 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""shebang"""
+
+
+def inherits_from(obj, a_class):
+    """check if obj inherits"""
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)
