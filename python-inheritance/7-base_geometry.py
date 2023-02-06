08e9a702a39205e85ee31e8917c1222ff089f42d
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-06 18:50:46 -0300

    task 7 test fixed

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -7,7 +7,7 @@ class BaseGeometry:
 
     def area(self):
         """returns exepction"""
-        raise Exception("area is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """"validates integer given"""
