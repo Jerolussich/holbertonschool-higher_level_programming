6c1002de4139e573a676ae7ef47e620d0b390667
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 11:15:36 -0300

    task 7 hotfix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
 filename = "add_item.json"
-with open(filename, 'w+', encoding="utf-8") as f:
+with open(filename, 'w', encoding="utf-8") as f:
     my_list = []
     my_list = args[1:]
     save_to_json_file(my_list, filename)
