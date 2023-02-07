0eb4299241584fcbaa69f2450c300190d5d8874c
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 10:28:15 -0300

    task 9 output hotfix

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 """shebang"""
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class BaseGeometry:
+    """BaseGeometry"""
+
+    def area(self):
+        """returns area"""
+        return (self.__width__ * self.__height__)
+
+    def integer_validator(self, name, value):
+        """integer validator given"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
-    """Rectangle class"""
+    """rectangle"""
 
     def __init__(self, width, height):
-        """initliazes class"""
+        """initialize class"""
         self.__width__ = width
         self.__height__ = height
         super().integer_validator("width", width)
         super().integer_validator("height", height)
 
     def __str__(self):
-        """prints string representation"""
+        """print string representation"""
         return f"[rectangle]<{self.__width__}>/<{self.__height__}>"
