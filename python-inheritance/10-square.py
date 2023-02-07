09c15b375e58bf51766365895f04e2c75002a2cc
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 10:39:31 -0300

    task 10 import changed

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,39 +1,7 @@
 #!/usr/bin/python3
 """shebang"""
 
-
-class BaseGeometry:
-    """class BaseGeometry"""
-
-    def area(self):
-        """returns area"""
-        return (self.__width__ * self.__height__)
-
-    def integer_validator(self, name, value):
-        """validates input given"""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """class Rectangle"""
-
-    def __init__(self, width, height):
-        """Function to initilize rectangle"""
-        super().integer_validator("width", width)
-        self.__width__ = width
-        super().integer_validator("height", height)
-        self.__height__ = height
-
-    def area(self):
-        """returns area"""
-        return self.__width__ * self.__height__
-
-    def __str__(self):
-        """returns atributes in string format"""
-        return f"[rectangle]<{self.__width__}>/<{self.__height__}>"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
