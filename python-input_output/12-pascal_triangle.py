3fd9febc16e5b534478fc03b76575ab78460e4d2
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 15:47:53 -0300

    final pep8 validation adjustment

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -12,14 +12,18 @@ def pascal_triangle(n):
         new_triangle[count][0] += 1
 
         for index, item in enumerate(triangle[count]):
-            if count == 2 and 1 <= index < len(new_triangle[count]) - 1:
-                new_triangle[count][index] += (new_triangle[count - 1][index - 1] +
-                                               new_triangle[count - 1][index - 1])
+            if (count == 2 and 1 <= index < len(new_triangle[count]) - 1):
+                new_triangle[count][index] += (
+                    new_triangle[count - 1][index - 1] +
+                    new_triangle[count - 1][index - 1]
+                )
             elif count >= 2 and index == len(new_triangle[count]) - 1:
                 new_triangle[count][index] += 1
             elif count > 2 and 1 <= index < len(new_triangle[count]) - 1:
-                new_triangle[count][index] += (new_triangle[count - 1][index] +
-                                               new_triangle[count - 1][index - 1])
+                new_triangle[count][index] += (
+                    new_triangle[count - 1][index] +
+                    new_triangle[count - 1][index - 1]
+                )
 
         if count == 1:
             new_triangle[count][1] += 1
