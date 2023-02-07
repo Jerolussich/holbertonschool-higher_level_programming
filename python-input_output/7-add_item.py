32f1c1964d0acf77fd9f5eca14ac7eccb44c3364
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 12:26:18 -0300

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,8 +9,14 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 args = sys.argv
 filename = "add_item.json"
-with open(filename, 'w', encoding="utf-8") as f:
-    my_list = []
-    my_list = args[1:]
-    save_to_json_file(my_list, filename)
+with open(filename, 'r+', encoding="utf-8") as f:
+    content = f.read()
+    formated_content = content.replace(
+        "[", "").replace("]", "").replace(",", "").replace("\"", "").replace(" ", "")
+
+    formated_content = formated_content.split(" ")
+    new_list = []
+    new_list = formated_content + args[1:]
+
+    save_to_json_file(new_list, filename)
     load_from_json_file(filename)
