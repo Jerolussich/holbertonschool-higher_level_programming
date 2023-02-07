666cd1ac64fe415c11355a06e258b9f88d1645e4
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 12:51:05 -0300

    task 7 hotfix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-    shebang
-
-"""
+"""shebang"""
 
 import sys
 
@@ -10,16 +7,23 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
-filename = "add_item.json"
-with open(filename, 'r+', encoding="utf-8") as f:
-    content = f.read()
-    formated_content = content.replace(
-        "[", "").replace("]", "").replace(",", "").replace("\"", "").replace(" ", "")
 
-    formated_content = formated_content.split(" ")
-    new_list = []
-    new_list = formated_content + args[1:]
+def main():
+    """main function"""
+    args = sys.argv
+    filename = "add_item.json"
+    with open(filename, 'r+', encoding="utf-8") as f:
+        content = f.read()
+        formated_content = content.replace(
+            "[", "").replace("]", "").replace(",", "").replace("\"", "").replace(" ", "")
 
-    save_to_json_file(new_list, filename)
-    load_from_json_file(filename)
+        formated_content = formated_content.split(" ")
+        new_list = []
+        new_list = formated_content + args[1:]
+
+        save_to_json_file(new_list, filename)
+        load_from_json_file(filename)
+
+
+if __name__ == "__main__":
+    main()
