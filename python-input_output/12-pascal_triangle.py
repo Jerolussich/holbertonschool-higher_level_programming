dd0e8430d18bcecc45701b8a088e1ed0cfa0871b
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 15:46:03 -0300

    task 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -3,28 +3,27 @@
 
 
 def pascal_triangle(n):
-    """create pascal triangle"""
+    """Create Pascal triangle."""
 
-    list = [[0 for _ in range(-1, i)] for i in range(n)]
-    new_list = list[:]
+    triangle = [[0 for _ in range(-1, i)] for i in range(n)]
+    new_triangle = triangle[:]
 
     for count in range(n):
+        new_triangle[count][0] += 1
 
-        new_list[count][0] += 1
-
-        for index, item in enumerate(list[count]):
-            if count == 2 and index >= 1 and len(new_list[count]) - 1 != index:
-                new_list[count][index] += new_list[count -
-                                                   1][index - 1] + new_list[count - 1][index - 1]
-            elif count >= 2 and index >= 1 and len(new_list[count]) - 1 == index:
-                new_list[count][index] += 1
-            elif count > 2 and index >= 1 and len(new_list[count]) - 1 != index:
-                new_list[count][index] += new_list[count -
-                                                   1][index] + new_list[count - 1][index - 1]
+        for index, item in enumerate(triangle[count]):
+            if count == 2 and 1 <= index < len(new_triangle[count]) - 1:
+                new_triangle[count][index] += (new_triangle[count - 1][index - 1] +
+                                               new_triangle[count - 1][index - 1])
+            elif count >= 2 and index == len(new_triangle[count]) - 1:
+                new_triangle[count][index] += 1
+            elif count > 2 and 1 <= index < len(new_triangle[count]) - 1:
+                new_triangle[count][index] += (new_triangle[count - 1][index] +
+                                               new_triangle[count - 1][index - 1])
 
         if count == 1:
-            new_list[count][1] += 1
+            new_triangle[count][1] += 1
 
-        list = new_list[:]
+        triangle = new_triangle[:]
 
-    return new_list
+    return new_triangle
