66c0a7d7547e1291a22c9629c4ed9af54a6ffb17
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 15:28:38 -0300

    pep8 fix

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -3,5 +3,6 @@
 
 
 def class_to_json(obj):
-    """ returns the dictionary description, for JSON serialization of an object:"""
+    """ returns the dictionary description, for 
+    JSON serialization of an object:"""
     return obj.__dict__
