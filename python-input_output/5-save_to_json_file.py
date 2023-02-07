44f83eafc632451d81f66ae414fb0388a762fbc7
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-07 11:02:49 -0300

    task 5

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""shebang"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """save json strucutre to file.txt"""
+
+    with open(filename, "w+") as file:
+        json.dump(my_obj, file)
+        file.close()
