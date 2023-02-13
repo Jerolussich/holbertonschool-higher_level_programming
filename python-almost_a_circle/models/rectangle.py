1cb0ef2024bcde80f0fd770b48f533de7720cff7
Jeronimo Lussich <jerolussich@hotmail.com>
2023-02-13 15:17:57 -0300

    rectangle output fix for checker2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -91,7 +91,7 @@ class Rectangle(Base):
 
     def __str__(self):
         """print string attributes"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.y} -\
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.y} - \
 {self.width}/{self.height}"
 
     def update(self, *args, **kwargs):
