31647e88775516e94fca53b0585e0142603307c3
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-10 10:06:49 -0300

    task 100 modified for checker

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,10 +1,14 @@
--- Encodes database to utf8
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Encodes database to utf8mb4
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-USE `hbtn_0c_0`;
-
-ALTER TABLE
-    `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0;
 
 ALTER TABLE
-    `first_table` CHANGE `name` `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE
+    first_table
+MODIFY
+    name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+SHOW CREATE TABLE first_table;
