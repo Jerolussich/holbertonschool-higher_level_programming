dffd88fe8a975018c75a3663bd453a6d41988dd1
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-14 15:47:14 -0300

    priviledges updated

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,4 +4,4 @@ CREATE USER IF NOT EXISTS 'user_0d_1' @'localhost';
 SET
     PASSWORD FOR 'user_0d_1' @'localhost' = 'user_0d_1_pwd;
 
-GRANT ALL PRIVILEGES ON *.* TO ' user_0d_1 ' @' localhost ';
+GRANT ALL ON *.* TO ' user_0d_1 ' @' localhost ';
