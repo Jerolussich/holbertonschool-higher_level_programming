e1b2ab459ef4653be5ac54a6f325709dcd7c722b
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-14 16:15:32 -0300

    tasks comments hotfix

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
---script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+-- Script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
 SELECT
     tv_shows.title,
     tv_genres.name
