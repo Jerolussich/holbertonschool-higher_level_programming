092afb688851091402fcb0136cdd665961f45bfc
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-14 16:23:15 -0300

    added capslock for checker

--- SQL_more_queries/102-rating_shows.sql
+++ SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Script that lists all shows from hbtn_0d_tvshows_rate by their rating.
 SELECT
     tv_shows.title,
-    SUM(ratings.rating) as rating_sum
+    SUM(ratings.rating) AS rating_sum
 FROM
     tv_shows
     JOIN ratings ON tv_shows.id = ratings.show_id
