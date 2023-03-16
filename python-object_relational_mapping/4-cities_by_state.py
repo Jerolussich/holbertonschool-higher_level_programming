820a46bbc49132cb9dfc215af9ee261dfb126d0c
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 02:11:59 -0300

    task 4 if statement and format added

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+"""script that lists all cities from the database hbtn_0e_4_usa"""
 
 import MySQLdb
 import sys
 
 db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     password="", database=sys.argv[3])
+                     password=sys.argv[2], database=sys.argv[3])
 
 
 if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id")
+        "SELECT cities.id, cities.name, states.name FROM cities\
+            INNER JOIN states ON states.id = cities.state_id ORDER BY cities.id")
 
     results = cursor.fetchall()
 
