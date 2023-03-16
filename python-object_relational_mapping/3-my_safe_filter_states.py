ee94ba0c3b43d883e5d53ebc1651b97803a75642
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 01:43:20 -0300

    task 3 checker test

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT * FROM states WHERE name LIKE '%s' ORDER BY states.id ASC".format(sys.argv[4]))
+        "SELECT * FROM states WHERE name LIKE '%s' \
+        ORDER BY states.id ASC".format(sys.argv[4]))
 
     result = cursor.fetchall()
 
