84453e2b4eec455ef96b08521e53eb0f357982c3
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 02:31:41 -0300

    task 6 hotfix

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-"""Start link class to table in database 
-"""
+"""Start link class to table in database"""
 import sys
 from model_state import Base
 from sqlalchemy import (create_engine)
