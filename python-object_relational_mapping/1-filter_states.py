dd6bb66c66f4d21e0190e27bff3055d7bc24d850
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 00:47:50 -0300

    task 1 format fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+        "SELECT id, name FROM states WHERE name LIKE \
+        'N%' ORDER BY states.id ASC")
 
     result = cursor.fetchall()
 
