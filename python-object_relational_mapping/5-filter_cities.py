9ba3d23f2de24f02ed62252d6f102a424658ca89
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 02:18:57 -0300

    task 5 reduced line length

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-"""script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa"""
-
-import MySQLdb
-import sys
-
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     password="", database=sys.argv[3])
-
+"""takes the name of a state as argument and lists all cities of that state"""
 
 if __name__ == "__main__":
 
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password=sys.argv[2], database=sys.argv[3])
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT cities.name FROM cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s ORDER BY cities.id", ((sys.argv[4]), ))
+        "SELECT cities.name FROM cities INNER JOIN states ON \
+        cities.state_id = states.id WHERE \
+        states.name = %s ORDER BY cities.id", ((sys.argv[4]), ))
 
     result = cursor.fetchall()
 
