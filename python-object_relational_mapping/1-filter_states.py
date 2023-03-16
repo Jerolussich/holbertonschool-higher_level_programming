3991c2b2fdfcea85149147283323b5c9d34bbc36
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 00:12:57 -0300

    task 1 limiter for import

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
+"""script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     password="", database=sys.argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password="", database=sys.argv[3])
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute(
-    "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    cursor.execute(
+        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
 
-result = cursor.fetchall()
+    result = cursor.fetchall()
 
-for item in result:
-    print(item)
+    for item in result:
+        print(item)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
