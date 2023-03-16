97e932909b281c0faceba93e775efa684367e759
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 00:51:05 -0300

    code not executable imported added

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -3,18 +3,19 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     password="", database=sys.argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password="", database=sys.argv[3])
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute(
-    "SELECT id, name FROM states WHERE name LIKE '%{}' ORDER BY states.id ASC".format(sys.argv[4]))
+    cursor.execute(
+        "SELECT id, name FROM states WHERE name LIKE '%{}' ORDER BY states.id ASC".format(sys.argv[4]))
 
-result = cursor.fetchall()
+    result = cursor.fetchall()
 
-for item in result:
-    print(item)
+    for item in result:
+        print(item)
 
-cursor.close()
-db.close()
+    cursor.close()
+    db.close()
