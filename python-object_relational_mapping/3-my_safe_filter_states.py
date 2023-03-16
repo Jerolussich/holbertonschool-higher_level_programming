ddfcdcf6a124011ef86c8192ec1b4fa44b9e5a75
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 01:37:34 -0300

    task 3 hotfix in string

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,13 +6,12 @@ import sys
 if __name__ == '__main__':
 
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password="", database=sys.argv[3])
+                         password=sys.argv[2], database=sys.argv[3])
 
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT id, name FROM states WHERE name LIKE %s \
-        ORDER BY states.id ASC".format(sys.argv[4]))
+        "SELECT * FROM states WHERE name LIKE '%s' ORDER BY states.id ASC".format(sys.argv[4]))
 
     result = cursor.fetchall()
 
