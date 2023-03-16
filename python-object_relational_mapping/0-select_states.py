66b3080775d9d31b2bf5f1cb0483db72e7fe38af
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 00:06:59 -0300

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,17 +4,18 @@
 import MySQLdb
 import sys
 
-db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                     password="", database=sys.argv[3])
+if __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         password="", database=sys.argv[3])
 
-cursor = db.cursor()
+    cursor = db.cursor()
 
-cursor.execute("""SELECT id, name FROM states
-         ORDER BY states.id ASC""")
+    cursor.execute("""SELECT id, name FROM states
+            ORDER BY states.id ASC""")
 
-result = cursor.fetchmany(5)
+    result = cursor.fetchmany(5)
 
-for item in result:
-    print(item)
-cursor.close()
-db.close()
+    for item in result:
+        print(item)
+    cursor.close()
+    db.close()
