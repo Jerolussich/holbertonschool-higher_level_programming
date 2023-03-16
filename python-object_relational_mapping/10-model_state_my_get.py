19d29f3b2a07338b09e1786b5774672155289dd0
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 04:09:10 -0300

    task 10 pycode style

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -11,7 +11,8 @@ import sys
 
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+                           (sys.argv[1], sys.argv[2],
+                            sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(engine)
     session = Session()
 
