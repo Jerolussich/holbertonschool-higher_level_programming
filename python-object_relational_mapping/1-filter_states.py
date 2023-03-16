88a77cceaa6ba0a14a0c2ebcdd98a6646b3527e1
Jeronimo Lussich <jerolussich@hotmail.com>
2023-03-16 00:45:14 -0300

    task 1 connect hotfix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -6,12 +6,12 @@ import sys
 
 if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         password="root", database=sys.argv[2])
+                         password="", database=sys.argv[3])
 
     cursor = db.cursor()
 
     cursor.execute(
-        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+        "SELECT id, name FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC")
 
     result = cursor.fetchall()
 
