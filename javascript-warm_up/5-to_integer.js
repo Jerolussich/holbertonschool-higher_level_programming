0e1534d92a5aebadda45981c4c09aaad0c6a804d
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-11 16:49:08 -0300

    task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv;
-const argumentGiven = Number(process.argv[2]);
+const argumentGiven = parseInt(process.argv[2]);
 
-if (!(argumentGiven != argumentGiven)) {
+if (!isNaN(argumentGiven)) {
   console.log(`My number: ${argumentGiven}`);
 } else {
   console.log('Not a number');
