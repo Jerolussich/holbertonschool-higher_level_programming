0fe7b2f82f3d785178097025f710c2c43677dc29
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-11 16:37:13 -0300

    removed length for checker

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+const to_print = args[0];
 
-const args = process.argv;
-const to_print = args[2];
-
-if ((args.length) === 2) { console.log('No argument'); } else { console.log(to_print); }
+if (to_print) {
+  console.log(to_print);
+} else {
+  console.log('No argument');
+}
