4af83c45350d84a5cbc93f68a46f8cb5b66745f8
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-11 16:44:27 -0300

    Added exception

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,4 +3,9 @@
 const args = process.argv;
 const argument_given = Number(process.argv[2]);
 
-if (!(argument_given != argument_given)) { console.log(`My number ${argument_given}`); }
+if (!(argument_given != argument_given)) { 
+    console.log(`My number: ${argument_given}`); 
+}
+else {
+    console.log('Not a number');
+}
