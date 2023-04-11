8b5d013e4d72465d9485d1ef068b7a8fa5a8f442
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-11 16:39:12 -0300

    Checker commit

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const to_print = args[0];
+const toPrint = args[0];
 
-if (to_print) {
-  console.log(to_print);
+if (toPrint) {
+  console.log(toPrint);
 } else {
   console.log('No argument');
 }
