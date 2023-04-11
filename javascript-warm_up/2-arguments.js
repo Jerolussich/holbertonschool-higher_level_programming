8673c7b61a1c72785960d0faa3a6fa152fce2e22
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-11 16:29:01 -0300

    removed console log command

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,10 +2,9 @@
 
 const args = process.argv;
 
-console.log(args)
-if ((args.length) == 2)
+if ((args.length) === 2)
     console.log('No argument');
-else if ((args.length) == 3)
+else if ((args.length) === 3)
     console.log('Argument found');
 else
     console.log('Arguments found');
