b073018398b3df55c24f8ed7125019b8a615097b
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-11 16:32:39 -0300

    Format fixed

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -2,9 +2,12 @@
 
 const args = process.argv;
 
-if ((args.length) === 2)
+if ((args.length) === 2) {
     console.log('No argument');
-else if ((args.length) === 3)
+}
+else if ((args.length) === 3) {
     console.log('Argument found');
-else
+}
+else {
     console.log('Arguments found');
+}
