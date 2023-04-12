d5f307af3db63b484e086b4f38429e26f9b3902a
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 10:36:56 -0300

    Format fixed

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -3,9 +3,13 @@
 const args = process.argv;
 const size = Number(args[2]);
 
-if ((size != size)) { console.log('Missing size'); } else {
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
   for (let i = 0; i < size; i++) {
-    for (let i = 0; i < size; i++) { process.stdout.write('X'); }
+    for (let j = 0; j < size; j++) {
+      process.stdout.write('X');
+    }
     console.log();
   }
 }
