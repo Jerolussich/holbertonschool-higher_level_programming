b1725a70c7e052e0a97db983194b65e6623edc8f
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 10:35:03 -0300

    Format fixed

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 
-
 const argumentGiven = Number(process.argv[2]);
 
-if ((argumentGiven !== argumentGiven)) { console.log('Missing number of occurrences'); } else {
-  for (let i = 0; i < argumentGiven; i++) { console.log('C is fun'); }
+if (isNaN(argumentGiven)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < argumentGiven; i++) {
+    console.log('C is fun');
+  }
 }
