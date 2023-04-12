20aa7189722b406014bfab3e7e4bc2b705b1a9e6
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 10:59:27 -0300

    added expections

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const args = process.argv;
+const args = process.argv.slice(2).map(Number);
 
-if (args.length < 4) { console.log('0'); } else {
-  let biggest_number = -Infinity;
-
-  for (let i = 2; i < args.length; i++) {
-    if (Number(args[i]) > biggest_number) { biggest_number = Number(args[i]); }
-  }
-  console.log(biggest_number);
+if (args.length === 0) {
+  console.log(0); 
+} else if (args.length === 1) {
+  console.log(0); 
+} else {
+  const sortedArgs = args.sort((a, b) => b - a); 
+  console.log(sortedArgs[1]); 
 }
