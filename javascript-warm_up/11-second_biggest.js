7ef1ba17f20fcf458ed37e4999739450f9d7c769
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 11:17:29 -0300

    Format fixed

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,10 @@
 const args = process.argv.slice(2).map(Number);
 
 if (args.length === 0) {
-  console.log(0); 
+  console.log(0);
 } else if (args.length === 1) {
-  console.log(0); 
+  console.log(0);
 } else {
-  const sortedArgs = args.sort((a, b) => b - a); 
-  console.log(sortedArgs[1]); 
+  const sortedArgs = args.sort((a, b) => b - a);
+  console.log(sortedArgs[1]);
 }
