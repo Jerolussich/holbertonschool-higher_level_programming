0e1face0e0943e4433c0870f0ed44cc5be955de9
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 10:50:53 -0300

    Function fixed

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
 
-const args = process.argv;
-
-console.log(factorial(Number(args[2])));
-
-function factorial (n) {
-  if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
-}
+console.log(parseInt(process.argv[2], 10) + parseInt(process.argv[3], 10));
