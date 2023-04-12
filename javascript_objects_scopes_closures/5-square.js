668221dc025215fc8e0f251ab658b1d2eced276f
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 11:52:29 -0300

    Broken checker, testing new format

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 const Rectangle = require('./4-rectangle');
 
 module.exports =
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-};
+  class Square extends Rectangle {
+    constructor (size) {
+      super(size, size);
+    }
+  };
