819ba3836d9e5a75da3c5f7cfeba3a8d3690685e
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 10:27:30 -0300

    hotfix

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const frases = ['C is fun','Python is cool','JavaScript is amazing\n'];
+const frases = ['C is fun', 'Python is cool', 'JavaScript is amazing\n'];
 
 frases.forEach(item => {
   console.log(item);
