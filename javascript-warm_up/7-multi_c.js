3e36573b49d6e01778086404420af6ef66eda64f
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-12 10:31:53 -0300

    Format fixed

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const args = process.argv;
-const argument_given = Number(process.argv[2]);
 
-if ((argument_given != argument_given)) { console.log('Missing number of occurrences'); } else {
-  for (let i = 0; i < argument_given; i++) { console.log('C is fun'); }
+const argumentGiven = Number(process.argv[2]);
+
+if ((argumentGiven !== argumentGiven)) { console.log('Missing number of occurrences'); } else {
+  for (let i = 0; i < argumentGiven; i++) { console.log('C is fun'); }
 }
