a84f73b2f154cb07e37c2076c6d2810763d754e9
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-18 13:49:22 -0300

    tasks 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -4,24 +4,28 @@ const request = require('request');
 const args = process.argv;
 
 const options = {
-    url: args[2],
-    method: `GET`
-}
-const user_data = {}
+  url: args[2],
+  method: 'GET'
+};
+
+const userData = {};
 
 request(options, function(err, resp, body) {
-    let data = JSON.parse(body)
+  if (err) {
+    console.log(err);
+    return;
+  }
+  
+  const data = JSON.parse(body);
 
-    data.forEach(task => {
-        
-        if (!user_data[task.userId])
-        {
-            user_data[task.userId] = 0;
-        }
-        if (task.completed == true)
-        {
-            user_data[task.userId]++;
-        }
-    });
-    console.log(user_data)
-})
+  data.forEach(task => {
+    if (!userData[task.userId]) {
+      userData[task.userId] = 0;
+    }
+    if (task.completed === true) {
+      userData[task.userId]++;
+    }
+  });
+
+  console.log(userData);
+});
