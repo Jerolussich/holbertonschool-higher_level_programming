ea14e794e35003a589f073a561f8b77c29f07255
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-18 13:41:25 -0300

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 const fs = require('fs');
 
-fs.readFile(args[2], 'utf-8', function(err, data) {
+fs.readFile(args[2], 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
