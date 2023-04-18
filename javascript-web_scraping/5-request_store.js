b08ca36003d9ad6f053b2a3becdf432bf1501899
Jeronimo Lussich <jerolussich@hotmail.com>
2023-04-18 13:47:44 -0300

    task 5

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -6,19 +6,20 @@ const fs = require('fs');
 const args = process.argv;
 
 const options = {
-    url: args[2],
-    method: 'GET'
+  url: args[2],
+  method: 'GET'
 };
 
-request(options, function(err, resp, body) {
+request(options, function (err, resp, body) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const data = String(body);
+  fs.writeFile(args[3], data, 'utf-8', function (err) {
     if (err) {
-        console.log(err);
-        return;
+      console.log(err);
     }
-    const data = String(body);
-    fs.writeFile(args[3], data, 'utf-8', function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
+  });
 });
